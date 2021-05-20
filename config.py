dbde89247ae51c157e0255279ea12b07124defa2
mahesh-baravkar <mahesh.baravkar@cuelogic.com>
2021-05-20 15:23:42 +0530

    scheduler chaneg

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ import routes
 from apidata import  CountrynStateData, DistrictsData
 
 scheduler1.add_job(func= CountrynStateData, trigger="interval", minutes=30)
+scheduler2.add_job(func= DistrictsData, trigger="interval", minutes=32)
 scheduler1.start()
-scheduler1.add_job(func= DistrictsData, trigger="interval", minutes=32)
-scheduler1.start()
+scheduler2.start()
 atexit.register(lambda: scheduler1.shutdown())
 atexit.register(lambda: scheduler2.shutdown())
